1cbd73a1756371a07632c1d48633ea89b8a8b705
ophidev <ophid7@gmail.com>
2025-10-04 07:41:01 +0530

    - /profile/view api call along with logic that without login you cannot redirect to any other routes. - If token is not present redirect user to /login page.

--- DevTinder-Frontend/src/components/Body.jsx
+++ DevTinder-Frontend/src/components/Body.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import { Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/Redux/userSlice";
 
 const Body = () => {
-  return (
-    <div className='w-screen h-screen overflow-hidden'>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const user = useSelector((appStore) => appStore?.user);
+  const navigate = useNavigate();
 
-export default Body
+  const fetchUser = async () => {
+    try {
+      if (user) return;
+
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
